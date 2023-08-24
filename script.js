03712ef6beece71e7613da058e761bda83692975
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-24 23:39:27 +0530

    to do list done

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let inputListTitle1 = document.querySelector(".input-list-title-1");
 let inputListTitle2 = document.querySelector(".input-list-title-2");
+let inputListTitle3 = document.querySelector(".input-list-title-3");
+let inputListTitle4 = document.querySelector(".input-list-title-4");
 let listItem1 = document.querySelectorAll(".list-item-1");
 let listItem2 = document.querySelectorAll(".list-item-2");
+let listItem3 = document.querySelectorAll(".list-item-3");
+let listItem4 = document.querySelectorAll(".list-item-4");
 let btnSaveList1 = document.querySelector(".savelist1");
-let btnClearList1 = document.querySelector(".clearlist1");
 let btnSaveList2 = document.querySelector(".savelist2");
+let btnSaveList3 = document.querySelector(".savelist3");
+let btnSaveList4 = document.querySelector(".savelist4");
+let btnClearList1 = document.querySelector(".clearlist1");
 let btnClearList2 = document.querySelector(".clearlist2");
+let btnClearList3 = document.querySelector(".clearlist3");
+let btnClearList4 = document.querySelector(".clearlist4");
 let msgboxgreen = document.querySelectorAll(".msg-box-green");
 let msgboxred = document.querySelectorAll(".msg-box-red");
 
@@ -95,11 +103,50 @@ function loadsecond(){
     loadlist(title2,items2,listItem2,inputListTitle2,1);
 }
 
+function thirdlistSave() {
+    let title3 = "title3";
+    let items3 = "item3";
+    saveList(title3,items3,listItem3,inputListTitle3,2);
+}
+function thirdlistClear(){
+    let title3 = "title3";
+    let items3 = "item3";
+    clearList(title3,items3,listItem3,inputListTitle3,2);
+}
+function loadthird(){
+    let title3 = "title3";
+    let items3 = "item3";
+    loadlist(title3,items3,listItem3,inputListTitle3,2);
+}
+
+
+function fourthlistSave() {
+    let title4 = "title4";
+    let items4 = "item4";
+    saveList(title4,items4,listItem4,inputListTitle4,3);
+}
+function fourthlistClear(){
+    let title4 = "title4";
+    let items4 = "item4";
+    clearList(title4,items4,listItem4,inputListTitle4,3);
+}
+function loadfourth(){
+    let title4 = "title4";
+    let items4 = "item4";
+    loadlist(title4,items4,listItem4,inputListTitle4,3);
+}
+
     
 loadfirst();
 loadsecond();
+loadthird();
+loadfourth();
 
 btnSaveList1.addEventListener("click",firstlistSave);
 btnClearList1.addEventListener("click",firstlistClear);
 btnSaveList2.addEventListener("click",secondlistSave);
 btnClearList2.addEventListener("click",secondlistClear);
+btnSaveList3.addEventListener("click",thirdlistSave);
+btnClearList3.addEventListener("click",thirdlistClear);
+btnSaveList4.addEventListener("click",fourthlistSave);
+btnClearList4.addEventListener("click",fourthlistClear);

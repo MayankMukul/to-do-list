26fd06bda47320e9bccab81a14359173f9958da7
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-24 11:50:00 +0530

    update

--- script.js
+++ script.js
@@ -2,46 +2,37 @@ let inputListTitle = document.querySelector(".input-list-title");
 let listItem = document.querySelectorAll(".list-item");
 let btnSaveList = document.querySelector(".savelist");
 let btnClearList = document.querySelector(".clearlist");
+let msgbox = document.querySelector(".msg-box");
 
 
 function loadlist() {
-    
-    
         if (localStorage.getItem("title")!==null && localStorage.getItem("items")!==null){
             let titledata = localStorage.getItem("title");
-            console.log(titledata);
             inputListTitle.value = titledata;
             
             let listData = localStorage.getItem("items");
             let data = listData.split(',');
             for (let i in data){
                 listItem[i].value = data[i];
-            }
-            
-
-        
+            }       
     }else {
         inputListTitle.value = "";
-        listItem[0].value = "";
-        listItem[1].value = "";
-        listItem[2].value = "";
-        listItem[3].value = "";
-        console.log("hello there ");
+        for (let i of listItem){
+            i.value = "";
+        }
     }
 }
 
 function saveList(){
-    console.log("title : "+inputListTitle.value);
     let dataArray = []
     for(let i of listItem){
         dataArray.push(i.value);
     }
-    console.log(dataArray);
 
     localStorage.setItem("title",inputListTitle.value);
     localStorage.setItem("items",dataArray);
+    msgbox.innerText = `Saved`;
 
-    console.log("local storage set");
 }
 
 function clearList(){

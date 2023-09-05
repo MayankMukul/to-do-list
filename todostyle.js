acc0a32df7154042c2219f5051a7df1fcfee1018
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-09-05 10:40:49 +0530

    load done

--- todostyle.js
+++ todostyle.js
@@ -1,7 +1,7 @@
 let addlistitem = document.querySelector("#add-list-item");
 let listgroup = document.querySelector(".list-group");
 let newlistitem ;
-let deleteItem = document.querySelector(".delete-item")
+let deleteItem = document.querySelectorAll(".delete-item")
 
 // let row = document.querySelector(".row");
 // let addmorelist = document.querySelector("#more-list");
@@ -15,12 +15,14 @@ function addItem() {
     <li class="list-group-item input-group">
         <input class="form-check-input checkbox me-1" type="checkbox"  />
         <input class="input-list list-item"  type="text" placeholder="Add Items Here..." />
-        <svg class="delete-item" onclick="deleteListItem()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+        <button class="delete-item btn" onClick="deleteListItem(this)">
+        <svg   xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
         </svg>
+        </button>
     </li>
     `;
-    console.log(count);
+    // console.log(count);
     
   listgroup.insertAdjacentHTML("beforeend", newlistitem);
 }
@@ -59,6 +61,8 @@ addlistitem.addEventListener("click",addItem);
 //     row.insertAdjacentHTML("afterend",newlist);
 // }
 // addmorelist.addEventListener("click",morelist);
+let listTitle=document.querySelector(".input-list-title");
+  let listItems=document.querySelectorAll(".list-item");
 
 function saveList(){
   let listTitle=document.querySelector(".input-list-title");
@@ -67,24 +71,24 @@ function saveList(){
   localStorage.setItem("count",listcount);
 
 
-  console.log("save running");
+  // console.log("save running");
   let cache = {};
   let n=0;
   listItems.forEach((a)=>{
-    console.log(n);
+    // console.log(n);
     cache[n]= a.value ;  //key value pair
-    console.log(JSON.stringify(a.value));
+    // console.log(JSON.stringify(a.value));
     n++;
   })
-  console.log(cache);
-  console.log(JSON.stringify(cache));
+  // console.log(cache);
+  // console.log(JSON.stringify(cache));
   let cachestring = JSON.stringify(cache);
   localStorage.setItem("cache",cachestring);
-  console.log(listTitle.value);
+  // console.log(listTitle.value);
 
-  console.log(JSON.stringify(listTitle.value));
-  // localStorage.setItem("Title",listTitle.value);
-  // localStorage.setItem("ListItem",JSON.stringify(listItems.value))
+  // console.log(JSON.stringify(listTitle.value));
+  localStorage.setItem("Title",listTitle.value);
+  
   
 
 }
@@ -93,20 +97,35 @@ savebtn.addEventListener("click",saveList);
 
 function loadListItem(){
   if(localStorage.getItem('cache')!=null){
+
+    listTitle.value = localStorage.getItem("Title");
+
   let strdata=localStorage.getItem('cache');
-  console.log(JSON.parse(strdata))
+  // console.log(JSON.parse(strdata))
   let data = JSON.parse(strdata);
 
-  let entrieslist = ( Object.values(data));
-  entrieslist.forEach((entries)=>{
-    console.log(`entries ${entries}`);
-  })
-  
   let newcount = localStorage.getItem("count");
- for(let a=0;a<newcount;a++){
- console.log("newcount");
- addItem();
+  for(let a=0;a<newcount;a++){
+//  console.log("newcount");
+  addItem();
  }
+
+ let newlistItems=document.querySelectorAll(".list-item");
+
+  let entrieslist = ( Object.values(data));
+  // console.log(entrieslist.length);
+  let arr = entrieslist;
+// console.log(newlistItems);
+ for(let r=0;r<entrieslist.length;r++ ){
+  newlistItems[r].value = arr[r];
+  // console.log(arr[r])
+ }
+
+  // entrieslist.forEach((entries)=>{
+  //   console.log(`entries ${entries}`);
+  // })
+  
+
 
   // console.log(entrieslist);
   // for(let a=0;a<entrieslist.length;a++){
@@ -118,7 +137,14 @@ function loadListItem(){
 loadListItem();
 
 
-function deleteListItem(){
-  console.log("delete this");
+function deleteListItem(e){
+  // console.log("delete this",e.parentNode);
+  listgroup.removeChild(e.parentNode);
+  count-=1;
+  
 }
-// deleteItem.addEventListener("click",deleteListItem);
+// for(a of deleteItem){
+//   a.addEventListener("click",deleteListItem);
+
+// console.log(a);
+// }
